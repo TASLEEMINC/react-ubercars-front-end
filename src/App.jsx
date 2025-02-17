cb2f7bc1a1354b12d52246e1e4dd66a156ccefa3
vitasmile <vitasmile@gmail.com>
2025-02-16 21:01:42 -0500

    Landing page and authorized user's dashboard are ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { useState, useEffect } from "react";
-import * as ubercarService from "./services/ubercarService";
-import UbercarList from "./components/UbercarList/UbercarList";
-import UbercarDetail from "./components/UbercarDetail/UbercarDetail";
-import UbercarForm from "./components/UbercarForm/UbercarForm";
 import { useContext } from 'react';
 import { Routes, Route } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
@@ -15,88 +11,7 @@ import { UserContext } from './contexts/UserContext';
 const App = () => {
   const { user } = useContext(UserContext);
 
-  const [ubercars, setUbercars] = useState([]);
-  const [selected, setSelected] = useState(null);
-  const [isFormOpen, setIsFormOpen] = useState(false);
 
-  useEffect(() => {
-    const fetchUbercars = async () => {
-      try {
-        const fetchedUbercars = await ubercarService.index();
-
-        if (fetchedUbercars.err) {
-          throw new Error(fetchedUbercars.err);
-        }
-
-        setUbercars(fetchedUbercars);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchUbercars();
-  }, []);
-
-  const handleSelect = (ubercar) => {
-    setSelected(ubercar);
-    setIsFormOpen(false);
-  };
-
-  const handleFormView = (ubercar) => {
-    if (!ubercar.id) setSelected(null);
-    setIsFormOpen(!isFormOpen);
-  };
-
-  const handleAddUbercar = async (formData) => {
-    try {
-      const newUbercar = await ubercarService.create(formData);
-
-      if (newUbercar.err) {
-        throw new Error(newUbercar.err);
-      }
-
-      setUbercars([newUbercar, ...ubercars]);
-      setIsFormOpen(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleUpdateUbercar = async (formData, ubercarId) => {
-    try {
-      const updatedUbercar = await ubercarService.update(formData, ubercarId);
-
-      if (updatedUbercar.err) {
-        throw new Error(updatedUbercar.err);
-      }
-
-      const updatedUbercarList = ubercars.map((ubercar) =>
-        ubercar.id !== updatedUbercar.id ? ubercar : updatedUbercar
-      );
-
-      setUbercars(updatedUbercarList);
-      setSelected(updatedUbercar);
-      setIsFormOpen(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleDeleteUbercar = async (ubercarId) => {
-    try {
-      const deletedUbercar = await ubercarService.deleteUbercar(ubercarId);
-
-      if (deletedUbercar.err) {
-        throw new Error(deletedUbercar.err);
-      }
-      setUbercars(
-        ubercars.filter((ubercar) => ubercar.id !== deletedUbercar.id)
-      );
-      setSelected(null);
-      setIsFormOpen(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <>
@@ -107,25 +22,7 @@ const App = () => {
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
-      <UbercarList
-        ubercars={ubercars}
-        handleSelect={handleSelect}
-        handleFormView={handleFormView}
-        isFormOpen={isFormOpen}
-      />
-      {isFormOpen ? (
-        <UbercarForm
-          handleAddUbercar={handleAddUbercar}
-          selected={selected}
-          handleUpdateUbercar={handleUpdateUbercar}
-        />
-      ) : (
-        <UbercarDetail
-          selected={selected}
-          handleFormView={handleFormView}
-          handleDeleteUbercar={handleDeleteUbercar}
-        />
-      )}
+      
     </>
   );
 };
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useContext, useState } from 'react'; //from here
 import { UserContext } from '../../contexts/UserContext';
 import * as userService from '../../services/userService';
+import UbercarList from '../UbercarList/UbercarList';
+import UbercarDetail from '../UbercarDetail/UbercarDetail';
+import UbercarForm from '../UbercarForm/UbercarForm';
+import * as ubercarService from "../../services/ubercarService.js";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
@@ -19,12 +23,114 @@ const Dashboard = () => {
     if (user) fetchUsers();
   }, [user]);
 
+  const [ubercars, setUbercars] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchUbercars = async () => {
+      try {
+        const fetchedUbercars = await ubercarService.index();
+
+        if (fetchedUbercars.err) {
+          throw new Error(fetchedUbercars.err);
+        }
+
+        setUbercars(fetchedUbercars);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchUbercars();
+  }, []);
+
+  const handleSelect = (ubercar) => {
+    setSelected(ubercar);
+    setIsFormOpen(false);
+  };
+
+  const handleFormView = (ubercar) => {
+    if (!ubercar.id) setSelected(null);
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddUbercar = async (formData) => {
+    try {
+      const newUbercar = await ubercarService.create(formData);
+
+      if (newUbercar.err) {
+        throw new Error(newUbercar.err);
+      }
+
+      setUbercars([newUbercar, ...ubercars]);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateUbercar = async (formData, ubercarId) => {
+    try {
+      const updatedUbercar = await ubercarService.update(formData, ubercarId);
+
+      if (updatedUbercar.err) {
+        throw new Error(updatedUbercar.err);
+      }
+
+      const updatedUbercarList = ubercars.map((ubercar) =>
+        ubercar.id !== updatedUbercar.id ? ubercar : updatedUbercar
+      );
+
+      setUbercars(updatedUbercarList);
+      setSelected(updatedUbercar);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeleteUbercar = async (ubercarId) => {
+    try {
+      const deletedUbercar = await ubercarService.deleteUbercar(ubercarId);
+
+      if (deletedUbercar.err) {
+        throw new Error(deletedUbercar.err);
+      }
+      setUbercars(
+        ubercars.filter((ubercar) => ubercar.id !== deletedUbercar.id)
+      );
+      setSelected(null);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
-      <p>
-        This is the dashboard page where you can see a list of all the users.
-      </p>
+      <h3>
+        This is the dashboard page where you can see a list of all the ubercars.
+      </h3>
+      <UbercarList
+        ubercars={ubercars}
+        handleSelect={handleSelect}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+        <UbercarForm
+          handleAddUbercar={handleAddUbercar}
+          selected={selected}
+          handleUpdateUbercar={handleUpdateUbercar}
+        />
+      ) : (
+        <UbercarDetail
+          selected={selected}
+          handleFormView={handleFormView}
+          handleDeleteUbercar={handleDeleteUbercar}
+        />
+      )}
     </main>
   );
 };
